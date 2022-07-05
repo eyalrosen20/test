3748e00600908cb0fd873b7558cb24c94c805144
chloerosen4 <chloe.rosen.4@gmail.com>
2022-07-06 00:11:45 +0300

    use socket

--- app.py
+++ app.py
@@ -1,66 +1,59 @@
-from asyncio import sleep
-import sched
 import numpy as np  
-from multiprocessing import Process, Pipe, Value, Manager
-from multiprocessing.connection import Listener
+import logging, time, timeit, os, socket, sys, socketserver
+import datetime as dt
+from multiprocessing import Process
 from multiprocessing.managers import BaseManager
-import more_itertools
-import os
-import time
-import timeit
-from apscheduler.schedulers.blocking import BlockingScheduler
+import multiprocessing as mp
 
 REQUIRED_RATE_IN_MS = 1
+HOST, PORT = "localhost", 4000
+NUM_VECTORS = 100
 
-def send_new_vector(p_input):
-  vector = np.random.default_rng().normal(size=(50,)) 
-  p_input.send_bytes(vector.tobytes())
-  time.sleep(0.01)
-
-def send_in_bindwidth(p_input):
+def send_in_bindwidth(request):
   # use wall time as opposed to cpu time to include IO wait time
-  inner_timeit = timeit.Timer(lambda: send_new_vector(p_input))
-  inner_time = inner_timeit.timeit(1)
-  time_to_hold = REQUIRED_RATE_IN_MS - inner_time
+  start_time = dt.datetime.now()
+  vector = np.random.default_rng().normal(size=(50,))
+  elapsed = (dt.datetime.now() - start_time).microseconds / 10000
+  time_to_hold = REQUIRED_RATE_IN_MS - elapsed 
   time.sleep(time_to_hold)
+  request.send(vector.tobytes())
+  elapsed = (dt.datetime.now() - start_time).microseconds / 10000
+  print(f"sending rate: {round(elapsed * 10, 2)} hertz")
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+  def handle(self):
+    self.data = self.request.recv(1024).strip()
+    send_in_bindwidth(self.request)
+    return
   
 def process1():
-  manager = BaseManager(address=('', 50000), authkey=b'abc')
-  server = manager.get_server()
-  server.serve_forever()
+  with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+    server.serve_forever()
 
-    outer_timeit = timeit.Timer(lambda: send_in_bindwidth(p_input))
-    outer_time = outer_timeit.timeit(1)
+def get_new_vector(sock):
+  start_time = dt.datetime.now()
+  sock.sendall(bytes("NEW_VECTOR\n", "utf-8"))
+  vector_bytes = sock.recv(1024)
+  vector = np.frombuffer(vector_bytes, dtype=np.float64)
+  elapsed = (dt.datetime.now() - start_time).microseconds / 10000
+  print(f"recieving rate: {round(elapsed * 10, 2)} hertz")
+  return vector
 
-    print("*** sending vector ***")
-    print(f"send rate: {(outer_time * 1000)} hertz")
-    print()
-    print()
-  # vectors_count = Value('d', 0)
-  # scheduler = BlockingScheduler()
-  # scheduler.add_job(send_new_vector, 'interval', [p_input, scheduler, vectors_count], seconds=1, id='new_vector_job')
+def process2():
+  time.sleep(2)
 
-
-def process2(p_output, p_input):
-    all_vectors = np.empty([100, 50], dtype=np.float64)
-    for i in range(5):
-      start_time = time.time()
-      vector_bytes = p_output.recv_bytes()
-      end_time = time.time()
-
-      response_time = (end_time - start_time) * 100
-      
-      vector = np.frombuffer(vector_bytes, dtype=np.float64)
+  all_vectors = np.empty([100, 50], dtype=np.float64)
+  for i in range(NUM_VECTORS):
+    print(f"*** recieving vector {i + 1} / {NUM_VECTORS} ***")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+      sock.connect((HOST, PORT))
+      vector = get_new_vector(sock)
       all_vectors[i] = vector
-
-      print(f"*** recieving vector {i + 1} / {5} ***")
-      print(f"response rate: {response_time * 1000} hertz")
-          
+    print()          
 
 if __name__ == '__main__':
-    p_output, p_input = Pipe(duplex=False)
-    p1 = Process(target=process1, args=(p_output, p_input),)
-    p2 = Process(target=process2, args=(p_output, p_input),)
+    p1 = Process(target=process1, args=(),)
+    p2 = Process(target=process2, args=(),)
     p1.start()
     p2.start()
     p1.join()
